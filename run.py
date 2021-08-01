77331a89754d333e6e919b17a4183191181b373b
JoelMichaelRutter <joel-rutter@live.co.uk>
2021-08-01 18:27:21 +0000

    add main_program_call function and welcome function to run.py

--- run.py
+++ run.py
@@ -13,6 +13,31 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("Learnpulse-Training-Register")
 
 training = SHEET.worksheet('register')
+# def input_data_to_register():
 
-training_data = training.get_all_values()
-print(training_data)
+def welcome_function():
+    """
+    This function is the welcome function, it is called
+    first in the main_program_call function. The function
+    prints a welcome message and instructions to the user.
+    """
+    print("WELCOME TO LEARNPULSE")
+    print("Through this program you can submit and search for \n"
+          "staff training information")
+    print("\n")
+    print("What would you like to do?")
+    print("Please enter one of the following options to progress:")
+    print('- Enter "input" to add trainee data to the database')
+    print('- Enter "search" to search for trainee data')
+
+def program_branch():
+
+def main_program_call():
+    """
+    This function is the main function in the program through which
+    all other functions are called.
+    """
+    welcome_function()
+
+
+main_program_call()
